518690d80d16bfaaf541cb476c65279113649603
pranjalidatascience <pgupta03@wm.edu>
2025-11-18 04:30:25 +0000

    sutip commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os 
+from langchain_core.messages import HumanMessage
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())  # read local .env file  
@@ -38,33 +39,58 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 import chainlit as cl
 
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+
+import chainlit as cl
+
+
 @cl.on_chat_start
 async def on_chat_start():
-    qa_system_prompt = """You are an assistant for question answering tasks \
-    Use the following pieces of retrieved context to answer the question \
-    If you don't know the answer, just say that you don't know \
-    Use six senetences maximum and keep the answer concise. \
-
-    {context}"""
-    qa_prompt = ChatPromptTemplate.from_messages(
+    model = ChatOpenAI(model="gpt-3.5-turbo",openai_api_key=OPENAI_API_KEY)
+    system_prompt = """ Given the chat history and a recent user question \
+    generate a standalone question \
+    that can be answered without the context of the chat history. DO NOT answer the question, \
+    just reformulate it if needed or otherwise return it as is. """
+    prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", qa_system_prompt),
-            MessagesPlaceholder("chat_history"),
-            ("human", "{input}")
+            ("system", system_prompt),
+            ("human", "{input}"),
         ]
     )
-
-    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    
+    retriever_with_history = create_history_aware_retriever(
+    llm, retriever, prompt
+    )
+    
+    # chat_history = []
+    # qa_system_prompt = """You are an assistant for question answering tasks \
+    # Use the following pieces of retrieved context to answer the question \
+    # If you don't know the answer, just say that you don't know \
+    # Use six senetences maximum and keep the answer concise. \
+    # {context}"""
+    
+    # qa_prompt = ChatPromptTemplate.from_messages(
+    #     [
+    #         ("system", qa_system_prompt),
+    #         MessagesPlaceholder("chat_history"),
+    #         ("human", "{input}")
+    #     ]
+    # )
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
 
     rag_chain=create_retrieval_chain(retriever_with_history, question_answer_chain)
-    cl.user_session.set("rag_chain", rag_chain)
+    
+    runnable = prompt | model | rag_chain | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
 
-# question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-# rag_chain=create_retrieval_chain(retriever_with_history, question_answer_chain)
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    runnable = cl.user_session.get("rag_chain")  # type: Runnable
+    runnable = cl.user_session.get("runnable")  # type: Runnable
 
     msg = cl.Message(content="")
 
@@ -75,6 +101,44 @@ async def on_message(message: cl.Message):
         await msg.stream_token(chunk)
 
     await msg.send()
+
+# @cl.on_chat_start
+# async def on_chat_start():
+#     qa_system_prompt = """You are an assistant for question answering tasks \
+#     Use the following pieces of retrieved context to answer the question \
+#     If you don't know the answer, just say that you don't know \
+#     Use six senetences maximum and keep the answer concise. \
+
+#     {context}"""
+#     qa_prompt = ChatPromptTemplate.from_messages(
+#         [
+#             ("system", qa_system_prompt),
+#             MessagesPlaceholder("chat_history"),
+#             ("human", "{input}")
+#         ]
+#     )
+
+#     question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+#     rag_chain=create_retrieval_chain(retriever_with_history, question_answer_chain)
+#     cl.user_session.set("rag_chain", rag_chain)
+
+# question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+# rag_chain=create_retrieval_chain(retriever_with_history, question_answer_chain)
+
+# @cl.on_message
+# async def on_message(message: cl.Message):
+#     runnable = cl.user_session.get("rag_chain")  # type: Runnable
+
+#     msg = cl.Message(content="")
+
+#     for chunk in await cl.make_async(runnable.stream)(
+#         {"question": message.content},
+#         config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+#     ):
+#         await msg.stream_token(chunk)
+
+#     await msg.send()
 # import textwrap 
 # from langchain_core.messages import HumanMessage
 # chat_history = []
